5b0b46d7fceb6259497573839e7d1f09705144a0
ZarakiLancelot <kenpachi2783@gmail.com>
2024-06-26 12:40:25 -0600

    carrousel, icon, font

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const buttons = document.querySelectorAll(".producto button");
+document.addEventListener('DOMContentLoaded', () => {
+  const carruseles = document.querySelectorAll('.carrusel');
 
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      alert("Producto agregado al carrito");
-    });
+  carruseles.forEach(carrusel => {
+    const slides = carrusel.querySelectorAll('.slide');
+    let index = 0;
+
+    // Función para mostrar el slide actual
+    function mostrarSlide() {
+      slides.forEach((slide, i) => {
+        if (i === index) {
+          slide.classList.add('active');
+          slide.classList.remove('inactive');
+        } else {
+          slide.classList.add('inactive');
+          slide.classList.remove('active');
+        }
+      });
+    }
+
+    // Cambiar de imagen cada 3 segundos
+    setInterval(() => {
+      index = (index + 1) % slides.length;
+      mostrarSlide();
+    }, 5000);
+
+    // Mostrar el primer slide al cargar la página
+    mostrarSlide();
   });
 });
